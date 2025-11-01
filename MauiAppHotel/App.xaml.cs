ced026747f0d67bd737cd2d6270db7389147c77e
Daniel179797 <daniel.jesus58@etec.sp.gov.br>
2025-10-31 22:15:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState);
+        {
+            var window = base.CreateWindow(activationState);
+
+        Window.width = 400;
+        Window.height = 600;
+
+        return window;
+        }
+
+    }
+}
